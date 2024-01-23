5d56bb91012858f63e0340f0b5e85f9bac88d4ab
Mo-theo <147115007+Mo-theo@users.noreply.github.com>
2024-01-23 15:18:29 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+// Defines the variable myAge
+let myAge = 23;
+// Defines the variable myAge
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+let laterYears = myAge-2;
+//Multiply the variable laterYears by four
+laterYears *= 4;
+// Defines the variable myAgeInDogYears
+let myAgeInDogYears = earlyYears+laterYears;
+
+
+let myName = "Theo".toLowerCase()
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
